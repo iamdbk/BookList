f3da1bec56e040a49ee4e95bd10ba28fcf2a45e5
Daivik Bijoy <iamdaivik@gmail.com>
2020-08-30 00:26:39 +0530

    Bottom portion is done, Most of the JS is also done, there is only a small bug to fixed. Its the alert part, and preventing it from from creating anymore rows for the table dynamically

---
+++ app.js
@@ -0,0 +1,54 @@
+// Book Constrcutor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Add Book to List
+UI.prototype.addBookTioLIst = function (book) {
+  const list = document.querySelector("#book-list");
+  const row = document.createElement("tr");
+  //    const colTitle = document.createElement("td"),
+  //     colAuthor = document.createElement("td"),
+  //     colIsbn = document.createElement("td"),
+  //     colCross = document.createElement("td");
+  //   colTitle.append(book.title);
+  //   colAuthor.append(book.author);
+  //   colIsbn.append(book.isbn);
+  //   colCross.append("X");
+  //   row.append(colTitle, colAuthor, colIsbn, colCross);
+  // list.append(row);
+  // OR
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td>X</td>
+ 
+    `;
+  list.append(row);
+};
+
+// Event Listeners
+document.querySelector("#book-form").addEventListener("submit", function (e) {
+  let title = document.querySelector("[title]").value,
+    author = document.querySelector("[author]").value,
+    isbn = document.querySelector("[isbn]").value;
+  if (title === "" || author === "" || isbn === "") {
+    alert("enter data properly");
+  }
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+  ui.addBookTioLIst(book);
+  (document.querySelector("[title]").value = ""),
+    (document.querySelector("[author]").value = ""),
+    (document.querySelector("[isbn]").value = "");
+  e.preventDefault();
+});
